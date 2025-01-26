cca5aae02f467fca0a099bc66ce02a5cf0ad4fec
ayush08negi <negi08ayush@gmail.com>
2025-01-26 16:12:30 +0530

    new changes

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -5,6 +5,7 @@ import { assets, jobsApplied } from '../assets/assets';
 import moment from 'moment'
 import AppContext from '../context/AppContext';
 import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const Applications = () => {
 
@@ -12,22 +13,26 @@ const Applications = () => {
 
   const [isEdit,setIsEdit] = useState(false);
   const [resume,setResume] = useState(null);
+  const[user,setUser] = useState(null);
 
 
   const updateResume = async()=>{
+    const data = JSON.parse(localStorage.getItem("userData"));
+    setUser(data)
+    console.log("userdata",user)
       try{
         const formData = new FormData()
         formData.append('resume',resume);
 
         const {data} = await axios.post(backendUrl + '/api/users/update-resume',
           formData,
-          {headers: {token:companyToken}}
+          {headers: {token: userToken}}
         )
 
         console.log("data",data);
 
         if(data.success){
-          toast.success(data.message)
+           toast.success(data.message)
            await fetchUserData();
         } else{
           toast.error(data.message)
@@ -48,14 +53,14 @@ const Applications = () => {
         <h2 className='text-xl font-semibold'>Your Resume</h2>
         <div className='flex gap-2 mb-6 mt-3'>
            {
-            isEdit || userData && userData.resume === ""
+            isEdit || user && user.resume === ""
             ? <>
              <label className='flex items-center' htmlFor="resumeUpload">
               <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2'>{resume ? resume.name : "Select Resume"}</p>
               <input id='resumeUpload' onChange={e => setResume(e.target.files[0])} accept='application/pdf' type="file" hidden />
               <img src={assets.profile_upload_icon} alt="" />
              </label>
-             <button onClick={e => setIsEdit(false)} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2 '>Save</button>
+             <button onClick={updateResume} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2 '>Save</button>
             </> :
             <div className='flex gap-2'>
               <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg 'href="">
